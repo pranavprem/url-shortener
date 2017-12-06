1ec28bd48f3f7a3390dfd4ece9589c7818dfb071
Pranav Prem <pranavprem93@gmail.com>
2017-12-06 00:48:26 -0800

    complete link redirect and control panel including ELK connection

--- linkredirect/service.py
+++ linkredirect/service.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 """Module that implements a link redirect engine for url shortener"""
+import json
 import boto3
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+
+
 
 def handler(event, context):
     """Event handler in lambda context"""
-    url = event["url"]
+    url = event["params"]["path"]["shorturl"]
     client = boto3.client("dynamodb")
+    esEndPoint = "search-url-shortener-t76w2u5nrhzsacc3n2oy3lvozi.us-west-1.es.amazonaws.com"
 
     try:
         item = client.get_item(TableName="urls", Key={
@@ -14,4 +19,14 @@ def handler(event, context):
     except Exception, exception:
         return exception
 
+    try:
+        esClient = Elasticsearch(
+            hosts=[{'host': esEndPoint, 'port': 443}],
+            use_ssl=True,
+            verify_certs=True,
+            connection_class=RequestsHttpConnection
+            )
+        esClient.index(index="urls", doc_type="hit", body=json.dumps(event))
+    except Exception as exception:
+        return exception
     return {"Location":"http://"+item["Item"]["url"]["S"]}
